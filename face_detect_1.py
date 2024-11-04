281bfd2920f7fe7ea7408f178fbefb4fb6d0bc58
abcuer <3486243105@qq.com>
2024-11-04 21:58:34 +0800

    基于opencv的人脸识别程序

--- face_detect_1.py
+++ face_detect_1.py
@@ -2,7 +2,7 @@ import cv2
 
 def detect_face(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_detect = cv2.CascadeClassifier('week_3/face_recognition_tools/haarcascade_frontalface_default.xml')
+    face_detect = cv2.CascadeClassifier('face_recognition_tools/haarcascade_frontalface_default.xml')
     face = face_detect.detectMultiScale(gray)
     for x,y,w,h in face:
         cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
@@ -14,7 +14,7 @@ def display(frame):
 
 
 if __name__ == '__main__':
-    img = cv2.imread('week_3/src/face_1.jpg')
+    img = cv2.imread('src/face_1.jpg')
     frame = cv2.resize(img, (1080, 960))
     img_detected = detect_face(frame)
     display(img_detected)
